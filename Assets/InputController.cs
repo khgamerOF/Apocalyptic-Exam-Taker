91a05b433e1487e75e3eb109b2c1e7107c648fa1
kylian-harmsen <D296238@edu.curio.nl>
2025-10-03 14:35:36 +0200

    porting of the game

---
+++ Assets/InputController.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputController : MonoBehaviour
+{
+    public static InputController instance;
+
+    private PlayerInput playerInput;
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        playerInput = GetComponent<PlayerInput>();
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public bool Jump()
+    {
+        float jumpFloat = playerInputActions.Player.SouthButton.ReadValue<float>();
+
+        if(jumpFloat >= 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      if (InputController.instance.Jump())
+        {
+            Debug.Log("Jump Key pressed");
+        }  
+    }
+}
