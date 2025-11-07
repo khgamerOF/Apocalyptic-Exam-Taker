b594099db44b151a94b53f0fe7a35c3389c65387
kylian-harmsen <D296238@edu.curio.nl>
2025-11-07 16:14:52 +0100

    added movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public InputAction playerControls;
+
+    Vector2 moveDirection = Vector2.zero;
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,9 +27,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      if (InputController.instance.Jump())
-        {
-            Debug.Log("Jump Key pressed");
-        }  
+        moveDirection = playerControls.ReadValue<Vector2>();
+    }
+    
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
