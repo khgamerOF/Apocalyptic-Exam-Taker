46ecbace393128cd1bd31ca30decaf9a0653f718
kylian-harmsen <D296238@edu.curio.nl>
2026-01-11 22:55:05 +0100

    camera follows player script

---
+++ Assets/scripts/CameraController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject player;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
