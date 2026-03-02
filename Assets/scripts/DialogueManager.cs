5a9ccf6d403e6c2bda91637dbb684c8e9e8ad1be
vio_volo_production <harmsenkylian@gmail.com>
2026-03-02 02:54:34 +0100

    removed unneeded assets + added the first dialog cutscene + added the empty scene for level 1 + import the dialog art for the protagonist

---
+++ Assets/scripts/DialogueManager.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Image characterImage;
+    public TextMeshProUGUI dialogueText;
+
+    public DialogueLine[] dialogueLines;
+
+    private int currentLine = 0;
+
+    void Start()
+    {
+        ShowLine();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) ||
+            Input.GetKeyDown(KeyCode.Z) ||
+            Input.GetButtonDown("Submit"))
+        {
+            NextLine();
+        }
+
+        if (Input.anyKeyDown)
+        {
+            Debug.Log("Key Pressed");
+        }
+    }
+
+    void ShowLine()
+    {
+        dialogueText.text = dialogueLines[currentLine].dialogueText;
+        characterImage.sprite = dialogueLines[currentLine].characterSprite;
+    }
+
+    void NextLine()
+    {
+        currentLine++;
+
+        if (currentLine < dialogueLines.Length)
+        {
+            ShowLine();
+        }
+        else
+        {
+            SceneManager.LoadScene("GameplayScene");
+        }
+    }
+}
